f0d9328ef074496cc4566c1771ba7ce259594e88
Gérald Leban <leban@insa-toulouse.fr>
2023-07-06 15:01:01 +0200

    chore(website): Change hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        brand: {
+          yellow: "#feea77",
+          orange: "#faab22",
+          pink: "#a34bb0",
+          purple: "#3614a6",
+          "purple-alt": "#a34bb0",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
